8e0acb202c001801db435cdea5ba19644f95f8a8
Ziyao Zheng <ziyaozheng@ziyaos-mbp.mynet>
2020-02-24 21:55:55 +0000

    #step 8 all tests passed, need to refacto

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,8 @@
 class Oystercard 
   DEFAULT_BALANCE = 0
   MAXIMUM_BALANCE = 90
-  attr_accessor :balance, :MAXIMUM_BALANCE
+  attr_accessor :balance, :MAXIMUM_BALANCE, :in_journey
+  
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end 
@@ -13,5 +14,17 @@ class Oystercard
 
   def deduct(fare)
     @balance -= fare
+  end 
+  
+  def in_journey?
+    @in_journey 
+  end  
+  
+  def touch_in
+    @in_journey = true
+  end 
+  
+  def touch_out
+    @in_journey = false
   end 
 end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe Oystercard do
     it 'has a default balance of zero' do 
       expect(subject.balance).to eq 0 
     end
- end 
+  end 
   
   describe '#top_up' do
+    it {is_expected.to respond_to(:top_up).with(1).argument}
     it 'tops up' do
       card = Oystercard.new
       card.top_up(10)
@@ -28,13 +29,31 @@ RSpec.describe Oystercard do
   end   
 
   describe '#deduct' do
-    it 'deducts when touch out' do
-    expect(subject).to respond_to(:deduct).with(1).argument
-    end
+    it {is_expected.to respond_to(:deduct).with(1).argument}
 
     it 'deducts the fare £2.4 per travel' do
       card = Oystercard.new(5)
       expect{card.deduct 2.4}.to change { card.balance }.by (-2.4)
     end  
-  end  
+  end 
+  
+  describe '#touch_in' do
+    # step1: before touching in, first ensure that the card is not in use
+    it 'is initially not in use' do
+      expect(subject).not_to be_in_journey
+    end 
+    # step2: after made sure the card is not in use, allow to touch in
+    it 'can touch in if not already touched in' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end 
+  end 
+  
+  describe '#touch_out' do 
+    it 'can touch out' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end 
 end 
