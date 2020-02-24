471f3d89981655c50fcc44b7078e78f1a89bb503
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-02-24 15:13:39 +0000

    #5 finished: top up function added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,12 @@
 class Oystercard 
   DEFAULT_BALANCE = 0
   attr_accessor :balance
+  
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+  end 
+
+  def top_up(amount)
+    @balance += amount
   end 
 end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,20 @@ RSpec.describe Oystercard do
   describe '#initialize' do
     it 'has a default balance of zero' do 
       expect(subject.balance).to eq 0 
-    end  
+    end
+ end 
+  
+  describe '#top_up' do
+    it 'tops up' do
+      card = Oystercard.new
+      card.top_up(10)
+      expect(card.balance).to eq 10
+    end 
+
+    it 'adds amount to current balance' do
+        card = Oystercard.new(5)
+      card.top_up(10)
+      expect(card.balance).to eq 15
+    end 
   end   
 end 
